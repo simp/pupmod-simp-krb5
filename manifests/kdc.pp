5c662bf1162a2fec30686c2abd1e3373296f8236
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-05-25 17:26:53 -0400

    (SIMP-991) Full Module Update
    * Update the structure of the Module
    * Add better spec tests
    * Add acceptance test suite for EL6 and EL7
    * Update documentation
    * Move RPM spec file to new format
    * Auto-initialize the system by default
    * Require Haveged by default
    * Fixed the native type (I have no idea how this ever worked)
    
    SIMP-991 #comment Full Module Update
    SIMP-1129 #close
    
    Change-Id: I69c8f60ea69b430fb4cce91a0ee777877f2b3ca0

--- manifests/kdc.pp
+++ manifests/kdc.pp
@@ -1,135 +1,113 @@
-# == Class krb5::kdc
-#
 # This class provides the necessary structure to manage the Kerberos 5 KDC on a
 # given system.
 #
 # The variables used here can be found in kdc.conf(5).
 #
-# Any variable *not* declared here can be managed using the augeas type.
-# However, you *must* target $krb5::kdc::kdc_conf_fragdir/defaults.conf as your
-# target in augeas!
+# Any variable *not* covered here can be managed using file resources.
 #
-# Also, make sure that your augeas resources notify Concat_build['kdc.conf'].
+# @example Add Your Own Custom Config Snippet
+#   class my_krb5kdc {
+#     include '::krb5::kdc'
 #
-# == Parameters
+#     file { "${::krb5::kdc::config_dir}/my_snippet__custom":
+#       content => "My Custom Content"
+#     }
 #
-# [*kdc_ports*]
-# [*kdc_tcp_ports*]
-# [*client_nets*]
-#   The client networks allowed to connect to the KDC.
+#     Class['krb5::kdc'] -> Class['my_krb5kdc']
+#     Class['my_krb5kdc'] ~> Class['krb5::kdc::service']
+#   }
 #
-# == Authors
+# @param client_nets [Array(NetworkAddress)] An Array of hostnames or IP
+#   addresses that are allowed into this system. Only used by the IPTables
+#   settings.
+# @param use_ldap [Boolean] If set, configure the system to incorporate LDAP
+#   components.
+#   @note This presently does not set up the LDAP back-end for KRB5
+# @param use_iptables [Boolean] If set, use the SIMP iptables module.
+# @param use_haveged [Boolean] If set, enable the HAVEGE daemon for entropy
+#   support.
+# @param auto_initialize [Boolean] If set, create a default realm do all
+#   necessary work to set up the environment for production.
+#   @note This will simply use the system defaults. If you want something other
+#     than that, you'll need to call the `::krb5::kdc::realm` define directly.
+#   @note If you select this, this *will* automatically initialize your
+#     Kerberos database and prepare your system to run. Passwords *will* be
+#     auto-generated using passgen() and used on the host system.
+# @param auto_realm [String] If $auto_initialize is set, then use this string
+#   as your default Kerberos Realm.
+# @param auto_management_principal [String] If $auto_initialize is set, then
+#   use this string as the primary Kerberos principal name for the default Realm.
+# @param auto_create_host_keytabs [Boolean] If `$auto_initialize` is set, then
+#   create keytabs for all hosts that Puppet currently knows about.
+#   @note This capability expects a `${module_name}_files` module to be present in
+#     the environment's module path. It is **not** recommended that you place
+#     this module inside of the standard module path. Instead, the containing
+#     directory should be added to the `modulepath` directive of your
+#     `environment.conf`.
+#      @see https://docs.puppet.com/puppet/4.5/reference/config_file_environment.html
+#        With the `${module_name}_files` module, you should also have a section in
+#        your Puppet auth.conf that looks something like the following and is
+#        placed **before** the `path /file` stanza.
 #
-# * Trevor Vaughan <tvaughan@onyxpoint.com>
+#     @example auth.conf update
+#       # Restrict access to a directory that matches the hostname
+#       # Example: /environments/production/krb5_files/files/my.host.name.domain
+#
+#       path ~ ^/file_(metadata|content)/modules/krb5_files/([^/]+)
+#       allow $2
+#
+# @author Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class krb5::kdc (
-  $kdc_ports = ['88','750'],
-  $kdc_tcp_ports = [],
-  $client_nets = hiera('client_nets')
-) {
+  $client_nets = hiera('client_nets', ['127.0.0.1']),
+  $config_dir = '/var/kerberos/krb5kdc/kdc.conf.simp.d',
+  $use_ldap = $::krb5::use_ldap,
+  $use_iptables = $::krb5::use_iptables,
+  $use_haveged = true,
+  $auto_initialize = true,
+  $auto_realm = $::domain,
+  $auto_management_principal = 'puppet_auto',
+  $auto_create_host_keytabs = true
+) inherits ::krb5 {
 
-  $l_kdc_ports = join($kdc_ports,',')
-  $kdc_conf_fragdir = fragmentdir('kdc.conf')
-
-  augeas { 'krb5_server_kdcdefaults_kdc_ports':
-    incl    => "${kdc_conf_fragdir}/defaults.conf",
-    lens    => 'Krb5Kdc.lns',
-    changes => [ "set kdcdefaults/kdc_ports '${l_kdc_ports}'" ],
-    require => [
-      Package['krb5-server'],
-      Concat_fragment['kdc.conf+defaults.conf'],
-      File['/usr/share/augeas/lenses/krb5kdc.aug']
-    ],
-    notify  => Concat_build['kdc.conf']
-  }
-
-  concat_build { 'kdc.conf':
-    file_delimiter => "\n\n",
-    target         => '/var/kerberos/krb5kdc/kdc.conf',
-    require        => Package['krb5-server'],
-    notify         => [
-      Service['krb5kdc'],
-      Service['kadmin']
-    ]
-  }
-
-  concat_fragment { 'kdc.conf+defaults.conf':
-    externally_managed => true
-  }
-
-  file { '/var/kerberos/krb5kdc':
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0640',
-    require => Package['krb5-server']
-  }
-
-  file { '/var/kerberos/krb5kdc/kdc.conf':
-    ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
-    notify => Concat_build['kdc.conf']
-  }
-
-  file { '/usr/share/augeas/lenses/krb5kdc.aug':
-    ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/krb5/krb5kdc.aug'
-  }
-
-  if !empty($kdc_tcp_ports) {
-    iptables::add_tcp_stateful_listen { 'allow_kdc':
-      order       => '11',
-      client_nets => $client_nets,
-      dports      => $kdc_tcp_ports
-    }
-  }
-
-  if !empty($kdc_ports) {
-    iptables::add_udp_listen { 'allow_kdc':
-      order       => '11',
-      client_nets => $client_nets,
-      dports      => $kdc_ports
-    }
-  }
-
-  # The ports for kadmind
-  iptables::add_udp_listen { 'allow_kadmind':
-    order       => '11',
-    client_nets => $client_nets,
-    dports      => ['464']
-  }
-  iptables::add_tcp_stateful_listen { 'allow_kadmind':
-    order       => '11',
-    client_nets => $client_nets,
-    dports      => ['464', '749']
-  }
-
-  krb5_acl { 'remove_default':
-    ensure         => 'absent',
-    principal      => '*/admin@EXAMPLE.COM',
-    operation_mask => '*'
-  }
-
-  package { 'krb5-server':
-    ensure => 'latest'
-  }
-
-  service { [
-      'krb5kdc',
-      'kadmin'
-    ]:
-    ensure     => 'running',
-    hasrestart => true,
-    hasstatus  => true,
-    require    => Package['krb5-server']
-  }
-
-  validate_array($kdc_ports)
-  validate_array($kdc_tcp_ports)
   validate_net_list($client_nets)
+  validate_absolute_path($config_dir)
+  validate_bool($use_ldap)
+  validate_bool($use_iptables)
+  validate_bool($use_haveged)
+  validate_bool($auto_initialize)
+  validate_string($auto_realm)
+  validate_string($auto_management_principal)
+  validate_bool($auto_create_host_keytabs)
+
+  if $use_haveged { include '::haveged' }
+
+  contain '::krb5::kdc::install'
+  contain '::krb5::kdc::config'
+  contain '::krb5::kdc::service'
+
+  Class['krb5'] -> Class['krb5::kdc']
+  Class['krb5::kdc::install'] ~> Class['krb5::kdc::config']
+  Class['krb5::kdc::install'] ~> Class['krb5::kdc::service']
+  Class['krb5::kdc::config'] ~> Class['krb5::kdc::service']
+
+  if $auto_initialize {
+    ::krb5::kdc::realm { $auto_realm:
+      initialize          => $auto_initialize,
+      auto_principal      => $auto_management_principal,
+      create_host_keytabs => $auto_create_host_keytabs
+    }
+
+    ::krb5::setting::realm { $auto_realm:
+      admin_server => $::fqdn,
+      kdc          => $::fqdn
+    }
+
+    Class['krb5::kdc::config'] -> Krb5::Kdc::Realm[$auto_realm]
+    Krb5::Kdc::Realm[$auto_realm] ~> Class['krb5::kdc::service']
+
+    if $use_haveged {
+      Class['haveged'] -> Krb5::Kdc::Realm[$auto_realm]
+    }
+  }
 }
--- spec/classes/keytab_spec.rb
+++ spec/classes/keytab_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe 'krb5::keytab' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
 
-  let(:facts) {{
-    :fqdn => 'test.example.domain'
-  }}
-
-  it { should create_class('krb5::keytab') }
-  it { should create_file('/etc/krb5_keytabs') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('krb5::keytab') }
+        it { is_expected.to create_file('/etc/krb5_keytabs') }
+      end
+    end
+  end
 end
