527c76550a0bfd5b5af5a24148db85651ef8ace6
op-ct <chris.tessmer@onyxpoint.com>
2020-08-05 12:45:22 -0400

    (SIMP-8139) Update to latest common assets (#56)

--- spec/fixtures/manifests/site.pp
+++
@@ -1,4 +0,0 @@
-# This file is normally a vestigial artifact whose presence is required by
-# rspec-puppet for direct spec testing (e.g., `rpec spec/classes/xxx_spec.rb`).
-#
-# Please keep this file to assist tools such as rspec and guard.
